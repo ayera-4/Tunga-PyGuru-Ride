808e947e282bf8cb0baffdd8811883675e0ce9a5
Evan Guma <gumaforevan@gmail.com>
2023-09-19 23:50:40 +0300

    Lists complete

--- String-manipulation_practice.py
+++ String-manipulation_practice.py
@@ -93,6 +93,7 @@ def word_occurrence(literal): # Number 8
 #input_sent = "Captain America is Captain of America"
 #print(word_occurrence(input_sent))
 
+
 def get_required_and_extra_fuel(distance): # Number 9
     min_fuel = 100
     required_fuel = distance * 10
@@ -104,6 +105,7 @@ def get_required_and_extra_fuel(distance): # Number 9
 #Test Output
 #test_distance = 0
 #print(get_required_and_extra_fuel(test_distance))
+
 
 def sums_even_and_odd(num_list): # Number 10
     even_sum = 0
@@ -118,6 +120,7 @@ def sums_even_and_odd(num_list): # Number 10
 #test_list = [51, 75, 69, 36, 75, 44, 82, 36]
 #print(sums_even_and_odd(test_list))
 
+
 def char_occurrence(literal): # Number 11
     char_list = []
     for char in literal.lower():
@@ -128,6 +131,7 @@ def char_occurrence(literal): # Number 11
 #Test Output
 #test_literal = "Nick Fury Hired Iron Man"
 #print(char_occurrence(test_literal))
+
 
 def is_abundant_number(number): # Number 12
     digit_sum = 0
@@ -141,6 +145,7 @@ def is_abundant_number(number): # Number 12
 #Test Output
 #number = 14
 #is_abundant_number(number)
+
 
 def is_sum_of_digits_equal(int1, int2): # Number 13
     int1_sum = 0
@@ -158,6 +163,7 @@ def is_sum_of_digits_equal(int1, int2): # Number 13
 #second_int = 900
 #print(is_sum_of_digits_equal(first_int, second_int))
 
+
 def repeat_vowels(literal, num): # Number 14
     vowels = ['a','e','i','o','u']
     new_literal = []
@@ -171,6 +177,7 @@ def repeat_vowels(literal, num): # Number 14
 #test_string = "banana"
 ##print(repeat_vowels(test_string, test_number))
 
+
 def count_digits_in_string(literal): # Number 15
     digit_count = 0
     for char in literal:
@@ -180,6 +187,7 @@ def count_digits_in_string(literal): # Number 15
 #Test Output
 #test_string = "Boys was released in 2019"
 #print(count_digits_in_string(test_string))
+
 
 def convert_camel_2_snake(literal): # Number 16
     if literal[0].islower() and "_" not in literal:
@@ -195,6 +203,7 @@ def convert_camel_2_snake(literal): # Number 16
 #Test Output
 #test_string = "helloworld"
 #print(convert_camel_2_snake(test_string))
+
 
 def is_lapindrome_number(num): # Number 17
     literal = str(num)
@@ -212,6 +221,7 @@ def is_lapindrome_number(num): # Number 17
 #Test Output
 #test_number = 1234321
 #print(is_lapindrome_number(test_number))
+
 
 def calculate_BMI(weight, height): # Number 18
     _BMI = round(weight/(height**2), 1)
@@ -240,6 +250,7 @@ def reverse_odd_long_words(literal): # Number 19
 #test_literal = "Learn to code from Programiz"
 #print(reverse_odd_long_words(test_literal))
 
+
 def ascending_squares(num_list): # Number 20
     square_list = [num**2 for num in num_list]
     square_list.sort()
@@ -247,6 +258,7 @@ def ascending_squares(num_list): # Number 20
 #Test Output
 #test_num_list = [-7,-2,0,4,5]
 #print(ascending_squares(test_num_list))
+
 
 def words_start_char(literal, char): # Number 21
     word_list = literal.split()
@@ -259,6 +271,7 @@ def words_start_char(literal, char): # Number 21
 #test_literal = "Doe went to Dubai"
 #test_char = 'd'
 #print(words_start_char(test_literal, test_char))
+
 
 def most_vowel_at_end(literal): # Number 22
     word_list = literal.split()
@@ -278,6 +291,7 @@ def most_vowel_at_end(literal): # Number 22
 #test_literal = "I owe you two hundred twenty two dollars"
 #print(most_vowel_at_end(test_literal))
 
+
 def build_readable_string(literal): # Number 23
     temp_literal = literal.strip().lower() + "."
     new_literal = temp_literal[0].upper() + temp_literal[1:]
@@ -286,6 +300,7 @@ def build_readable_string(literal): # Number 23
 #test_literal = "  hEllo worlD iN Python  "
 #print(build_readable_string(test_literal))
 
+
 def check_sequence(num_list): # Number 24
     init_list = num_list.copy()
     num_list.sort()
@@ -293,9 +308,6 @@ def check_sequence(num_list): # Number 24
         return True
     else:
         return False
-    return num_list
-    #print(init_list)
-    #print(num_list)
 #Test Output
 #number_list = [1,2,3,4,5]
 #print(check_sequence(number_list))
---
+++ lists_practice.py
@@ -0,0 +1,130 @@
+def print_list_items(item_list): #Number 1
+    for item in item_list:
+        print(item)
+
+#fruits = ["apple", "kiwi", "grapes", "mango"]
+#print_list_items(fruits)
+
+
+def add_to_list_end(odd_list): #Number 2
+    try:
+        number = int(input("Enter another odd number: "))
+        if number % 2 == 1:
+            odd_list.append(number)
+            print(odd_list)
+        else:
+            print("Input is not an odd number")
+    except ValueError:
+        print("Input is not an odd number")
+        return 0
+
+#odd_numbers = [1,3,5]
+#print(f"Current odd number list: {odd_numbers}")
+#add_to_list_end(odd_numbers)
+
+
+def calculate_product(item_list): #Number 3
+    product = 1
+    for item in item_list:
+        product *= item
+    return print(product)
+
+#my_list = [2,5,3,4,1]
+#calculate_product(my_list)
+
+
+def check_first_and_last(literal): #Number 4
+    char_list = list(literal)
+    if char_list[0] == char_list[-1]:
+        return print("Equal")
+    else:
+        return print("Not Equal")
+
+#check = "banana"
+#check_first_and_last(check)
+
+
+def print_odd_numbers(num_list): #Number 5
+    for num in num_list:
+        if num % 2 == 1:
+            print(num)
+
+#numbers = [1,2,3,4,5,6,7,8]
+#print_odd_numbers(numbers)
+
+
+def reverse_list(num_list): #Number 6
+    return print(num_list[::-1])
+#my_list = [1,2,3,4]
+#reverse_list(my_list)
+
+
+def create_new_list(char_list): #Number 7
+    return print(char_list[1:-1])
+
+#my_list = ['p','y','t','h','o','n']
+#create_new_list(my_list)
+
+
+def sum_even_and_odd(num_list): #Number 8
+    odd_sum = 0
+    even_sum = 0
+    for num in num_list:
+       if num % 2 == 0:
+           even_sum += num
+       else:
+           odd_sum += num
+    return (odd_sum, even_sum)
+
+#test_list = [51,75,69,36,75,44,82,36]
+#print(sum_even_and_odd(test_list))
+
+
+def square_numbers(num_list): #Number 9
+    square_list = []
+    for num in num_list:
+        square_list.append(num**2)
+    square_list.sort()
+    return square_list
+
+#test_list = [-7,-2,0,4,5,7]
+#print(square_numbers(test_list))
+
+
+def check_sequence(num_list): #Number 10
+    benchmark_list = num_list.copy()
+    num_list.sort()
+    if num_list == benchmark_list:
+        return True
+    else:
+        return False
+#test_list = [1,2,3,4,5]
+#print(check_sequence(test_list))
+
+
+def print_char_occurrence(literal, in_char): #Number 11
+    count = 0
+    for char in literal:
+        if char == in_char:
+            count += 1
+    return count
+
+#string1 = input("Enter a string for check: ")
+#character1 = input("Enter a character to be checked: ")
+#print(print_char_occurrence(string1, character1))
+
+
+def replace_yt(literal): #Number 12
+    language = "Python"
+    language = language.replace("yt", literal)
+    return language
+#ch = input("Enter a string: ")
+#print(replace_yt(ch))
+
+
+def concatenate_strings(literal1, literal2):
+    result = literal1 + literal2
+    return result
+#string1 = input("Enter first string")
+#string2 = input("Enter second string")
+#print(concatenate_strings(string1, string2))
