052aab0740c9dab7c3533b44ca8bb0b683425ab2
Evan Guma <gumaforevan@gmail.com>
2023-09-20 13:32:52 +0300

    Intermediate functions done

--- functions_practice.py
+++ functions_practice.py
@@ -27,9 +27,47 @@ def my_function(num_A, num_B): #Number 3
 #my_function(num1, num2)
 
 
-def full_name(first_name, last_name):
+def full_name(first_name, last_name): #Number 4
     print(f"{first_name} {last_name}")
 
 #first = input("Enter your first name: ")
 #last = input("Enter your last name: ")
 #full_name(first, last)
+
+
+#def check_prime(num): #Number 5
+
+
+def calculate_pow(arg1): #Number 6
+    return print(arg1**3)
+
+#input_num = int(input("Enter an integer: "))
+#calculate_pow(input_num)
+
+
+def display_info(name, location): #Number 7
+    print(name)
+    print(location)
+#country = input("Enter country of location: ")
+#display_info(name="Magnus",location=country)
+
+
+def find_sum(n): #Number 8
+    total = 0
+    for i in range(1, n+1):
+        total += i
+    return print(total)
+
+#number = int(input("Enter an integer: "))
+#find_sum(number)
+
+
+def compute_area(radius, pi): #Number 9
+    area = pi * (radius ** 2)
+    return print(area)
+
+#circle_rad = float(input("Enter radius of the circle: "))
+#circle_pi = 3.14
+#compute_area(circle_rad, circle_pi)
+
+
